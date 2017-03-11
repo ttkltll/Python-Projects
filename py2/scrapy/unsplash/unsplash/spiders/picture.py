23a0f4b2f055bb0c5e67afd6f084facc0af28ae0
swee <1158341873@qq.com>
2017-03-11 13:02:54 +0800

    crawling unsplash use scrapy v1.0

--- py2/scrapy/unsplash/unsplash/spiders/picture.py
+++ py2/scrapy/unsplash/unsplash/spiders/picture.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 from scrapy.spiders import BaseSpider
 import scrapy
 import json
@@ -6,12 +7,13 @@ from unsplash.items import UnsplashItem
 class PictureSpider(scrapy.spiders.Spider):
     name = "picture"
     allowed_domains = ["unsplash.com"]
-    start_urls = [
+    start_urls = [  #爬虫最先爬取的链接
         "https://api.unsplash.com/napi/feeds/home"
     ]
 
     def parse(self, response):
-        item = UnsplashItem()
+        item = UnsplashItem()   #创建item实例
+        #获取下一页的跳转url
         res = response.body
         hjson = json.loads(res)
         next_page = hjson[u'next_page']
@@ -19,6 +21,7 @@ class PictureSpider(scrapy.spiders.Spider):
         page_bianhao = re.findall(pattern, next_page)[0]
         page_url = 'https://api.unsplash.com/napi/feeds/home?after=' + page_bianhao
         print page_url
+        #获取当前页中图片的url
         photos = hjson[u'photos']
         pic_urls=[]
         for each in photos:
@@ -26,5 +29,5 @@ class PictureSpider(scrapy.spiders.Spider):
             pic_urls.append('https://unsplash.com/photos/' + bianhao + '/download?force=true')
         item['pic_urls']=pic_urls
         print item['pic_urls']
-        yield item
-        yield scrapy.Request(page_url, callback=self.parse)
+        yield item  #传给item，进行后续存储处理
+        yield scrapy.Request(page_url, callback=self.parse) #加入后续爬取的链接列表
